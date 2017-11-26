921917aefc6ffcc887eb5d4e6ca7f7882c4eb0cf
Bruno Holliger <bholliger@gmx.net>
2017-11-26 21:12:15 +0100

    Add files via upload

---
+++ Set-SoundDevice3CX/Get-SoundDevice3CX.ps1
@@ -0,0 +1,23 @@
+﻿#Requires -version 5.1
+<#
+    .SYNOPSIS 
+    Get a list of sound devices
+
+    .DESCRIPTION
+    Get a list of sound devices
+
+    .NOTES
+    Get a list of sound devices
+                
+    .EXAMPLE
+    .\Get-SoundDevice3CX.ps1
+
+#>
+# Version 0.5/26.11.2017
+# Copyright ©2017 Bruno Holliger, Switzerland
+
+# List all audio devices
+Get-PnpDevice | Where-Object {$_.InstanceId -like 'SWD\MMDEVAPI\*' -and $_.Status -eq 'OK' -and $_.Present -eq $true -and $_.PNPClass -eq 'AudioEndpoint'} | Select-Object FriendlyName | Out-GridView
+
+Write-Host 'Hit enter to close the window.'
+Read-Host
